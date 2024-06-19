ab06a4cdc4196977077b319f44e3cd295e0ed1d4
Diego Cortes <diegocortes.pinilla@gmail.com>
2024-06-18 23:19:32 -0400

    htmls cargados

--- myapp/urls.py
+++ myapp/urls.py
@@ -4,5 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.index,name='index'),
     path('about/',views.about,name='about'),
-
+    path('galeria/',views.galeria,name='galeria'),
+    path('formulario/',views.formulario,name='formulario'),
+    path('noticias/',views.noticias,name='noticias'),
+    path('api/',views.api,name='api'),
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -8,3 +8,15 @@ def index (request):
     
 def about (request):
     return render(request, 'myapp/about.html')
+
+def galeria (request):
+    return render(request, 'myapp/galeria.html')
+
+def formulario (request):
+    return render(request, 'myapp/formulario.html')
+
+def noticias (request):
+    return render(request, 'myapp/noticias.html')
+
+def api (request):
+    return render(request, 'myapp/api.html')
