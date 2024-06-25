ce6f034eeb5ef7330efe13dfbdd3614ec0910520
Maxi <ma.reszczynski@duocuc.cl>
2024-06-25 16:42:10 -0400

    ULTIMO COMMIT

--- myapp/views.py
+++ myapp/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from .models import Usuario, Producto, Venta
 from .forms import UsuarioForm
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
@@ -16,11 +18,7 @@ def galeria (request):
 
 def formulario (request):
     if request.method != 'POST':
-        usuario = Usuario.objects.all()
-        context = {
-            "usuario": Usuario,
-        }
-        return render(request, "myapp/formulario.html", context)
+        return render(request, "myapp/formulario.html")
     else:
         nombre = request.POST["nombre"]
         apellido = request.POST["apellido"]
@@ -49,8 +47,32 @@ def noticias (request):
 def api (request):
     return render(request, 'myapp/api.html')
 
-def login (request):
-    return render(request, 'myapp/login.html')
+def login(request):
+    if request.method=="POST":
+        #Corresponde al formulario
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request,username=username,password=password)
+        if user is not None:
+            login(request,user)
+            usuarios = Usuario.objects.all()
+            context = {
+                "usuarios":usuarios,
+            }
+            return render(request,"myapp/index.html",context)
+        else:
+            context = {
+                "mensaje":"Usuario o contraseña incorrecta",
+                "design":"alert alert-danger w-50 mx-auto text-center",
+            }
+            return render(request,"myapp/login.html",context)
+    else:
+        #Corresponde a redireccionar
+        context = {
+        }
+        return render(request,"myapp/login.html",context)
+
+
 
 def tienda (request):
     return render(request, 'myapp/tienda.html')
